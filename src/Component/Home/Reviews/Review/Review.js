751340e41cb5e57aa366cd189b19a121aae525b8
Ashraful610 <ashalam610@gmail.com>
2022-09-19 20:46:43 +0600

    review page is done

--- src/Component/Home/Reviews/Review/Review.js
+++ src/Component/Home/Reviews/Review/Review.js
@@ -1,10 +1,22 @@
 import React from 'react';
 
-const Review = () => {
+const Review = ({review}) => {
+    const {img , description , rating , name} = review;
     return (
-        <div>
-            
-        </div>
+        <div className=" text-white  backdrop-blur-sm bg-white/30">
+          {/* -- ---------- img div -----------*/}
+            <div className="img-div flex justify-center p-4 ">
+                <img src={img} className='w-28 h-28 rounded-full' alt="" />
+            </div>
+            {/* ------- review text -----------*/}
+            <div className="content-text text-center  px-2">
+                <p className="py-2">{description}</p>  
+                <h2>Rating : {rating} </h2>
+                 <h3 className="font-semibold text-lg py-2 font-serif bg-clip-text text-transparent bg-gradient-to-r from-pink-300 to-sky-500">
+                     {name}
+                </h3>
+            </div>
+    </div>
     );
 };
 
--- src/Component/Home/Reviews/Reviews.js
+++ src/Component/Home/Reviews/Reviews.js
@@ -1,10 +1,31 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Review from './Review/Review'
 
 const Reviews = () => {
+    const [reviews, setReviews] = useState([])
+    
+    useEffect(() =>{
+         fetch('reviews.json')
+         .then(res => res.json())
+         .then(data => setReviews(data))
+     },[])
     return (
-        <div>
-            
-        </div>
+    <main className="w-full min-h-screen h-fit sm:px-10 sm:pb-10 md:px-8 p-5 bg-black/90 ">
+          <div className="sm:p-5 p-0">
+             <h2 className='sm:text-5xl text-3xl text-center  font-semibold font-serif bg-clip-text text-transparent bg-gradient-to-r from-pink-600 to-sky-500'>
+                Our Customers Reviews
+             </h2>
+          </div>
+        {/* -------- review section --------------------*/}
+        <section className="w-full h-5/6 flex items-center">
+            <div className="w-full h-full  lg:p-5 md:py-0 py-5 grid md:grid-cols-3 gap-5">
+                {
+                    reviews.map(review => <Review key={review.img} review={review}></Review>)
+                }
+              
+            </div>
+        </section>
+   </main>
     );
 };
 
